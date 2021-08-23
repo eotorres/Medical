ac83d8993f93b946c665a8ef9512d233591d1dba
Emmanuel Orestes Torres <65071460+eotorres@users.noreply.github.com>
2021-08-23 13:03:39 -0300

    Update medical.py

--- medical.py
+++ medical.py
@@ -104,7 +104,7 @@ elif pagina == 'Modelagem de valor do seguro':
 
 		s1 = 'Custo Estimado do Seguro (Pred): ${:.2f}'.format(pred)
 		s2 = 'Valor de Venda do Seguro: ${:.2f}'.format(valor)
-		s3 = 'Calculo:'+(x*s1)'
+		s3 = 'Calculo:'.format(x)
 		
 		
 		st.markdown('## Resultados do modelo para as entradas:')
@@ -112,6 +112,7 @@ elif pagina == 'Modelagem de valor do seguro':
 		st.markdown('## **' + s1 + '**') 
 		st.markdown('## **' + s2 + '**')
 		st.markdown('## **' + s3 + '**')
+		st.markdown('## **' + s3 * s1 + '**')
 
 ###### PAGINA: MODELO DE FRAUDE ######
 
